e4635ec254d6c76d54e3151db8fa318bbffefae3
Ubuntu <book@Ubuntu.(none)>
2014-11-03 20:49:13 +0800

    add print.c

---
+++ print.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{ int i,j,k;
+  for(i=0;i<4;i++) 
+  { 
+   for(j=0;j<=2-i;j++) 
+   printf(" "); 
+   for(k=0;k<=2*i;k++) 
+   printf("*"); 
+   printf("\n"); 
+  } 
+}
